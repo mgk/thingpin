587c4d37a2708f728b5d4e81284b38218d52acae
Michael Keirnan <michael@keirnan.com>
2015-11-02 18:44:44 -0500

    Bump version: 1.0.0 → 1.1.0-dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,14 @@ This project uses [Semantic Versioning](http://semver.org/).
 
 ### Fixed
 - bugs
+### Added
+- stuff
+
+### Changed
+- things
+
+### Fixed
+- bugs
 ## [1.0.0](https://github.com/mgk/thingpin/releases/tag/v1.0.0) - [2015-11-01]
 ### Added
 - stuff
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='thingpin',
-    version='1.0.0',
+    version='1.1.0-dev',
     description='Raspberry Pi pin monitor that reports to AWS IoT',
     long_description=__doc__,
     url='https://github.com/mgk/thingpin/blob/master/README.md',
